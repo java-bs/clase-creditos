a5ddddc495fe22953b5fd77cf73067d49a4f6b00
Jorge Porto <jporto@bsaenz.com.ar>
2019-11-19 11:06:16 -0300

    Upload inicial

---
+++ src/main/java/com/clasecreditos/clasecreditos/Main.java
@@ -0,0 +1,11 @@
+package com.clasecreditos.clasecreditos;
+
+
+public class Main {
+
+
+    public static void main(String[] parametros) {
+        System.out.println("Hello Moon!");
+    }
+    
+}
